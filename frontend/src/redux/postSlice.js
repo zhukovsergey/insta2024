be47fd51c8b132c0bf30b091b53df8dcffbfca67
zhukovsergey <zhukov@etalon48.com>
2024-09-12 16:43:15 +0300

    last22222

--- frontend/src/redux/postSlice.js
+++ frontend/src/redux/postSlice.js
@@ -3,26 +3,17 @@ const postSlice = createSlice({
   name: "post",
   initialState: {
     posts: [],
-<<<<<<< HEAD
     selectedPost: null,
-=======
->>>>>>> 976d9f8b94fc195d3520a860a6d9ff83471f8b91
   },
   reducers: {
     //actions
     setPosts: (state, action) => {
       state.posts = action.payload;
     },
-<<<<<<< HEAD
     setSelectedPost: (state, action) => {
       state.selectedPost = action.payload;
     },
   },
 });
 export const { setPosts, setSelectedPost } = postSlice.actions;
-=======
-  },
-});
-export const { setPosts } = postSlice.actions;
->>>>>>> 976d9f8b94fc195d3520a860a6d9ff83471f8b91
 export default postSlice.reducer;
